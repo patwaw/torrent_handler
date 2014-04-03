d5ac6a12aba4d18d7c3cfa8211994442ddd36ee0
patwaw <zuberekk@gmail.com>
2014-04-03 19:53:12 +0200

    Added parser, renamed some things

--- torrent_handler/main.py
+++ torrent_handler/main.py
@@ -9,31 +9,37 @@
 # TODO: More automation with torrents
 # TODO: MIME type torrent add
 
+import sys
+import argparse
 import transmissionrpc
 
 class Torrent(object):
     def __init__(self, host='localhost', port=9091):
         self.host = host        
         self.port = port        
-        self.client = transmissionrpc.Client(self.host, self.port)
+        try:
+            self.client = transmissionrpc.Client(self.host, self.port)
+        except:
+            print "Connection refused. Exiting"
+            sys.exit(1)
 
-    def info_all(self):
+    def list(self):
         '''
         Function prints every torrent information.
         '''
         for torrent in self.client.get_torrents():
-            print torrent.id
+            print torrent.id, torrent.status
             print torrent.name
             print torrent.format_eta
             print "\n"
 
-    def info_one(self, torrent_id):
+    def info(self, torrent_id):
         '''
         Function prints information about single torrent
         '''
         self.torrent_id = torrent_id
         self.result = self.client.get_torrent(self.torrent_id)
-        print self.result.id
+        print self.result.id, self.result.status
         print self.result.name
 
     def add_torrent(self, url):
@@ -47,11 +53,31 @@ class Torrent(object):
         '''
         Function remove torrent from client
         '''
-        self.torrent_id = torrent_id
         self.data_remove = data_remove
         return self.client.remove_torrent(self.torrent_id, self.data_remove)
 
 	
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description="torrent_handler")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-a", "--add", type=str)
+    group.add_argument("-r", "--remove", type=int)
+    group.add_argument("-i", "--info", type=int)
+    group.add_argument("-l", "--list", action='store_true')
+    parser.add_argument('-p', '--port', type=str, help="Specify Transmission Remote port. default=9091")
+    parser.add_argument('-c', '--connect', type=str, help="Specify Transmission Remote hostname/ip. default=localhost")
+    args = parser.parse_args()
+    port = args.port
+    host = args.connect
+    client = Torrent(host, port)
+    if args.add != None:
+        client.add_torrent(args.add)
+    elif args.remove != None:
+        client.remove_torrent(args.remove)
+    elif args.list != None:
+        client.list()
+    elif args.info != None:
+        client.info(args.info)
+    else:
+        sys.exit(0)
